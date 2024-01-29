8e7797255ce3f647da7f349e95522433df7404c9
Nick0086 <nikunjkalathiya86@gmail.com>
2024-01-29 13:56:23 +0530

    complate

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,66 +1,80 @@
 import React, { useState } from 'react'
 import "./Form.css"
 import Input from '../Input';
+import axios from 'axios';
+
 
 function Form() {
 
     const data = {
-        name:"",
-        email:"",
-        address:"",
-        password:"",
+        name: "",
+        email: "",
+        address: "",
+        password: "",
     }
-    const [userData,setUserData] = useState(data);
+    const [userData, setUserData] = useState(data);
 
     const userdataHandler = (e) => {
-        const {name,value} = e.target;
+        const { name, value } = e.target;
         setUserData({ ...userData, [name]: value })
 
     }
 
-    const dataHandler = (e) => {
+    const dataHandler = async (e) => {
         e.preventDefault();
-        console.log("userData",userData)
+        console.log("userData", userData)
+
+        await axios({
+            method: 'post',
+            url: 'http://localhost:4000/',
+            userData,
+        })
+            .then(function (response) {
+                console.log("response", response);
+            })
+            .catch(function (error) {
+                console.log("error", error);
+            })
     }
 
-  return (
-    <>
-      <form className='form-body' onSubmit={dataHandler} >
+    return (
+        <>
+            <form className='form-body' method="post" onSubmit={dataHandler} >
 
-        <Input
-          label="Name"
-          type="text"
-          name="name"
-          value={userData.name}
-          onChange={userdataHandler}
-        />
-        <Input
-          label="Email"
-          type="email"
-          name="email"
-          value={userData.email}
-          onChange={userdataHandler}
-        />
-        
-        <Input
-          label="Address"
-          type="text"
-          name="address"
-          value={userData.address}
-          onChange={userdataHandler}
-        />
-        
-        <Input
-          label="Password"
-          type="password"
-          name="password"
-          value={userData.password}
-          onChange={userdataHandler}
-        />
-        <button type="submit" className='submit-btn' >Submit</button>
-      </form>
-    </>
-  )
+                <Input
+                    label="Name"
+                    type="text"
+                    name="name"
+                    value={userData.name}
+                    onChange={userdataHandler}
+                />
+                <Input
+                    label="Email"
+                    type="email"
+                    name="email"
+                    value={userData.email}
+                    onChange={userdataHandler}
+                />
+
+                <Input
+                    label="Address"
+                    type="text"
+                    name="address"
+                    value={userData.address}
+                    onChange={userdataHandler}
+                />
+
+                <Input
+                    label="Password"
+                    type="password"
+                    name="password"
+                    value={userData.password}
+                    onChange={userdataHandler}
+                />
+                <button type="submit" className='submit-btn' >Submit</button>
+            </form>
+        </>
+    )
 }
 
 export default Form;
